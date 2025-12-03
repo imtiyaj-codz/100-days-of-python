8e22c42282e26720224fa036b23daf4eb83d6044
imtiyaj-codz <imtiyaz.maniyar07@gmail.com>
2025-12-03 21:08:43 +0530

    Hangman project

--- hangman.py
+++ hangman.py
@@ -1,16 +1,27 @@
 import random
-word_list=["Dog","Cat","Bird"]
+
+word_list=["dog","cat","bird"]
 
 chosen_word=random.choice(word_list)
+print(chosen_word)
+print(chosen_word.replace(chosen_word,"_"))
+
+guess=input("Guess the letter :").lower()
+
+# for letter in chosen_word:
+#     if guess == letter:
+#         print("Right")
+#     elif guess == letter:
+#         print("Right")
+#     elif guess == letter:
+#         print("Right")
+#     elif guess == letter:
+#         print("Right")
+#     else:
+#         print("Wrong")
 
 for letter in chosen_word:
-    print(letter)
-
-if letter =="D":
-    print("correct")
-if letter =="O":
-    print("correct")
-if letter =="G":
-    print("correct")
-else:
-    print("Wrong")
+    if guess == letter:
+        print("Right")
+    else:
+        print("Wrong")
