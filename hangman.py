c4e31ceacdc79701b7b232f3b1508fd1af9322ad
imtiyaj-codz <imtiyaz.maniyar07@gmail.com>
2025-12-05 20:45:59 +0530

    Practice logic

--- hangman.py
+++ hangman.py
@@ -5,11 +5,13 @@ word_list=["dog","cat","bird"]
 chosen_word=random.choice(word_list)
 print(chosen_word)
 
-placeholder= ""
-word_list=len(chosen_word)
-for i in range(word_list):
+placeholder="" #This wors as an empty container here to store the udnerscore
+word_length=len(chosen_word) #This is to convert the str into interger so that it can run
+for i in range(word_length): #the i variable goes from the selected word from the word_list letter by letter to replace it with the underscore
     placeholder+="_ "
+
 print(placeholder)
+
 
 guess=input("Guess the letter :").lower()
 display=""
