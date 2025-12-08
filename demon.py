21d852adab40aa73d1b054d39e0932ac7e0c99cc
imtiyaj-codz <imtiyaz.maniyar07@gmail.com>
2025-12-08 21:09:43 +0530

    types of functions

---
+++ demon.py
@@ -0,0 +1,4 @@
+def add(a, b):
+    return a + b
+sum = add(5, 10)
+print(sum)   # 15
--- para_fun.py
+++ para_fun.py
@@ -1,1 +1,35 @@
-print("Functions with parameters")
+# def greet():
+#     print("Hello guys ")
+#     print("Hello welcome to kopargaon guys")
+#     print("Enjoy the kopargaon guys")
+# greet()
+
+def greet_with_name(name):
+    print(f"Hello guys {name}")
+    print(f"Hello welcome to kopargaon {name}")
+greet_with_name("Serena")
+
+
+def add(a,b):
+    print(a+b)
+
+add(5,10)
+
+def full_name(first, last):
+    print(f"{first} {last}")
+
+full_name("Imtiyaz", "Maniyar")
+
+
+def greet(name="Friend"):
+    print("Hello", name)
+
+greet()         # prints: Hello Friend
+greet("Moin")   # prints: Hello Moin
+
+
+def add(a, b):
+    return a + b
+sum = add(5, 10)
+print(sum)   # 15
+
