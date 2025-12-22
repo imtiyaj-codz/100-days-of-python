394794a001023d29b1583225a99e7e70907a1177
imtiyaj-codz <imtiyaz.maniyar07@gmail.com>
2025-12-22 21:45:58 +0530

    Dictonaries_practice

--- student_scores.py
+++ student_scores.py
@@ -8,5 +8,20 @@ student_scores = {
 
 student_grades = {}
 
+for name in student_scores:
+    marks=student_scores[name]
 
-
+    if marks >=91 and marks <=100:
+        student_grades[name]="Outstanding"
+    elif marks >=81 and marks <=90:
+        student_grades[name]="Exceeds expectation"
+    elif marks >=71 and marks <=80:
+        student_grades[name]="Acceptable"
+    else:
+        student_grades[name]="Fail"
+    
+print(student_grades)
+# Scores 91 - 100: Grade = "Outstanding"
+# Scores 81 - 90: Grade = "Exceeds Expectations"
+# Scores 71 - 80: Grade = "Acceptable"
+# Scores 70 or lower: Grade = "Fail"
