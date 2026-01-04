612a7156ebba48b4475963b66b24a89c6f236e47
imtiyaj-codz <imtiyaz.maniyar07@gmail.com>
2026-01-04 21:12:32 +0530

    Leet Code Problems solving

---
+++ Object Oriented Programming in Python/employee_raise.py
@@ -0,0 +1,13 @@
+class Employee:
+    raise_amount = 1.04
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+
+    def fullname(self):
+        return f"{self.first} {self.last}"
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
