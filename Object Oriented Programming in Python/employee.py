c45ec771a083bb191b36125d2c6ec4f4632ba6c5
imtiyaj-codz <imtiyaz.maniyar07@gmail.com>
2026-01-09 21:35:59 +0530

    alternative methods in python

--- Object Oriented Programming in Python/employee.py
+++ Object Oriented Programming in Python/employee.py
@@ -1,18 +1,23 @@
-print("This is an program demonstrating the use of classes,instances and methods in python\n")
-
 class Employee():
-    def __init__(self,fname,lname,pay):
-        self.fname=fname
-        self.lname=lname
-        self.pay=pay
-        self.email=fname+"."+lname+"@gmail.com"
+    raise_amount = 1.04 #class variable
 
-    def fullname(self):
-        return '{} {}'.format(self.fname,self.lname)
+    def __init__(self,first,last,pay):#Constructor class automatically created
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email= self.first+"."+self.last + '@gmail.com'
+    def fullname(self):#Method
+        return '{} {}'.format(self.first+"."+self.last)
+    def apply_raise(self): #use of class variable and can be accessed by any class in the program
+        self.pay=int(self.pay * self.raise_amount)
+        #You can use class variable either by class name (Employee.raise_amount or by instance (self.raise_amount))
 
-emp_1=Employee('Alain','Wesker',50000)
-emp_2=Employee('Ash','Ketchup',65000)
 
-print(emp_1.email)
-print(emp_1.fullname())
-print(emp_2.fullname())
+emp_1=Employee("alain", "wesker", 50000)# Instance/Object 
+
+# you can use this method to print the attrubutes of class and object from the method or constructor
+print(emp_1.pay)
+
+# This is the use of the class variable apply raise
+emp_1.apply_raise()
+print(emp_1.pay)
