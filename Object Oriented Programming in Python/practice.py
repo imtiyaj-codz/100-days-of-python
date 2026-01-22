0138ad6b6ab58559317e519c82790d3546c9c448
imtiyaj-codz <imtiyaz.maniyar07@gmail.com>
2026-01-22 20:45:08 +0530

    sql foreign keys

--- Object Oriented Programming in Python/practice.py
+++ Object Oriented Programming in Python/practice.py
@@ -1,3 +1,4 @@
+import datetime
 class Employee():
     raise_amount = 1.04
 
@@ -16,6 +17,18 @@ class Employee():
     @classmethod
     def set_raise_amount(cls,amount):
         cls.raise_amount = amount
+    
+    @classmethod #Alternative Constructor
+    def from_string(cls,emp_str):
+        first , last , pay = emp_str.split('-')
+        return cls (first , last , pay)
+
+    @staticmethod
+    def work_day(day):
+        if day == 0 or day == 7:
+            return False
+        return True
+
 
 emp_1 = Employee("Alain", "Wesker", 50000)
 emp_2 = Employee("Serena", "Williams", 76000)
@@ -25,3 +38,16 @@ Employee.set_raise_amount(1.05)
 print(Employee.raise_amount)
 print(emp_1.raise_amount)
 print(emp_2.raise_amount)
+
+emp_str_1 = 'Ashley-Benstokes-70000'
+emp_str_2 = 'Clemont-Zelonski-37703'
+emp_str_3 = 'Ashley-Anderson-60000'
+
+new_emp_1 = Employee.from_string(emp_str_1)
+
+print(new_emp_1.email)
+print(new_emp_1.pay)
+
+my_day = datetime.date(2025 , 7 , 7)
+
+print(Employee.work_day(my_day))
