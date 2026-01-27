05c9c9c7935c8c0a03240fd86d55e5d80619f544
imtiyaj-codz <imtiyaz.maniyar07@gmail.com>
2026-01-27 21:31:15 +0530

    introduction to microsoft excel

---
+++ Data Structures in Python/tim_sort.py
@@ -0,0 +1,55 @@
+def insertion_sort(arr, left, right):
+    for i in range(left + 1, right + 1):
+        key = arr[i]
+        j = i - 1
+        while j >= left and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+def merge(arr, l, m, r):
+    left = arr[l:m + 1]
+    right = arr[m + 1:r + 1]
+
+    i = j = 0
+    k = l
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            arr[k] = left[i]
+            i += 1
+        else:
+            arr[k] = right[j]
+            j += 1
+        k += 1
+
+    while i < len(left):
+        arr[k] = left[i]
+        i += 1
+        k += 1
+
+    while j < len(right):
+        arr[k] = right[j]
+        j += 1
+        k += 1
+
+def tim_sort(arr):
+    n = len(arr)
+    RUN = 32
+
+    for i in range(0, n, RUN):
+        insertion_sort(arr, i, min(i + RUN - 1, n - 1))
+
+    size = RUN
+    while size < n:
+        for left in range(0, n, 2 * size):
+            mid = min(left + size - 1, n - 1)
+            right = min(left + 2 * size - 1, n - 1)
+            if mid < right:
+                merge(arr, left, mid, right)
+        size *= 2
+
+    return arr
+
+arr = [5, 21, 7, 23, 19]
+print(tim_sort(arr))
--- Object Oriented Programming in Python/inheritance.py
+++ Object Oriented Programming in Python/inheritance.py
@@ -8,22 +8,25 @@ class Employee():
         self.email = first + '.' + last + "@gmail.com"
 
     def fullname(self):
-        return '{} {}'.format(self.first,self.last)
+        return '{} {}'.format(self.first, self.last)
     
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amount)
     
     @classmethod
-    def set_raise_amount(cls,amount):
+    def set_raise_amount(cls, amount):
         cls.raise_amount = amount
 
 class Developer(Employee):
     raise_amount = 1.10
+    
+    def __init__(self, first ,last, pay, prog_lang):
+        super().__init__(first ,last, pay)
+        self.prog_lang = prog_lang
 
-dev_1 = Developer("Alain", "Wesker", 50000)
-dev_2 = Developer("Serena", "Williams", 60000)
+dev_1 = Developer("Alain", "Wesker", 50000 , "Java")
+dev_2 = Developer("Serena", "Williams", 60000 , "Python")
 
-print(dev_1.pay)
-dev_1.apply_raise()
-print(dev_1.pay)
+print(dev_1.prog_lang)
+print(dev_2.prog_lang)
 
