e29999e242c3b8949e73caceb82944e85f7e7768
imtiyaj-codz <imtiyaz.maniyar07@gmail.com>
2026-01-29 21:07:20 +0530

    sql injection

---
+++ Data Structures in Python/cocktail.py
@@ -0,0 +1,31 @@
+def cocktail_sort(arr):
+    n = len(arr)
+    start = 0
+    end = n - 1
+    swapped = True
+
+    while swapped:
+        swapped = False
+
+        for i in range(start, end):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                swapped = True
+
+        if not swapped:
+            break
+
+        swapped = False
+        end -= 1
+
+        for i in range(end, start, -1):
+            if arr[i] < arr[i - 1]:
+                arr[i], arr[i - 1] = arr[i - 1], arr[i]
+                swapped = True
+
+        start += 1
+
+    return arr
+
+arr = [5, 1, 4, 2, 8, 0, 2]
+print(cocktail_sort(arr))
