7e204e42de02b647109130aaf9fe768192b732ed
I-nullptr <imtiyaz.maniyar07@gmail.com>
2026-03-28 23:19:11 +0530

    Add customer and payment tables with join queries
    Create tables for customers and payments, insert sample data, and perform various join queries with filtering.

---
+++ SQL Programs/Customer_join_query_w_filtering.sql
@@ -0,0 +1,44 @@
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50)
+);
+
+CREATE TABLE payments (
+    payment_id INT PRIMARY KEY,
+    customer_id INT,
+    amount INT
+);
+
+INSERT INTO customers VALUES
+(1, 'Imtiyaz', 'Shaikh'),
+(2, 'Rahul', 'Patil'),
+(3, 'Sara', 'Khan'),
+(4, 'Aman', 'Verma');
+
+INSERT INTO payments VALUES
+(1, 1, 500),
+(2, 2, 300),
+(3, 2, 200),
+(4, 5, 700);
+
+SELECT customers.first_name, payments.amount
+FROM customers
+INNER JOIN payments
+ON customers.customer_id = payments.customer_id;
+
+SELECT customers.first_name, payments.amount
+FROM customers
+LEFT JOIN payments
+ON customers.customer_id = payments.customer_id;
+
+SELECT customers.first_name, payments.amount
+FROM customers
+RIGHT JOIN payments
+ON customers.customer_id = payments.customer_id;
+
+SELECT customers.first_name, payments.amount
+FROM customers
+INNER JOIN payments
+ON customers.customer_id = payments.customer_id
+WHERE payments.amount < 400;
