25b66c0f9ec1c387789ca60048548514a6f6e307
I-nullptr <imtiyaz.maniyar07@gmail.com>
2026-04-02 22:27:53 +0530

    Add customers and payments tables with sample data
    Create tables for customers and payments, insert sample data, and query top customers by total spending.

---
+++ SQL Programs/Top_customers.sql
@@ -0,0 +1,38 @@
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50)
+);
+
+CREATE TABLE payments (
+    payment_id INT PRIMARY KEY,
+    customer_id INT,
+    amount INT
+);
+
+INSERT INTO customers VALUES
+(1, 'Imtiyaz', 'Shaikh'),
+(2, 'Rahul', 'Patil'),
+(3, 'Sara', 'Khan'),
+(4, 'Aman', 'Verma');
+
+INSERT INTO payments VALUES
+(1, 1, 500),
+(2, 2, 300),
+(3, 2, 200),
+(4, 5, 700);
+
+SELECT customers.customer_id, customers.first_name, SUM(payments.amount) AS total_spent
+FROM customers
+INNER JOIN payments
+ON customers.customer_id = payments.customer_id
+GROUP BY customers.customer_id, customers.first_name
+ORDER BY total_spent DESC;
+
+SELECT customers.customer_id, customers.first_name, SUM(payments.amount) AS total_spent
+FROM customers
+INNER JOIN payments
+ON customers.customer_id = payments.customer_id
+GROUP BY customers.customer_id, customers.first_name
+ORDER BY total_spent DESC
+LIMIT 2;
