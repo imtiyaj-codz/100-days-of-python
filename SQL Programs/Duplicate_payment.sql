e56c8fbf9674a5b2de351e0091ed1916ea0a5d4f
I-nullptr <imtiyaz.maniyar07@gmail.com>
2026-04-21 22:47:59 +0530

    Add Duplicate_payment.sql with customer and payment data
    Create tables for customers and payments, insert sample data, and query for duplicate payments.

---
+++ SQL Programs/Duplicate_payment.sql
@@ -0,0 +1,46 @@
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50)
+);
+
+CREATE TABLE payments (
+    payment_id INT PRIMARY KEY,
+    customer_id INT,
+    amount INT
+);
+
+INSERT INTO customers VALUES
+(1, 'Imtiyaz', 'Shaikh'),
+(2, 'Rahul', 'Patil'),
+(3, 'Sara', 'Khan'),
+(4, 'Aman', 'Verma');
+
+INSERT INTO payments VALUES
+(1, 1, 500),
+(2, 1, 500),
+(3, 2, 300),
+(4, 2, 300),
+(5, 3, 400);
+
+SELECT customers.first_name, payments.amount,
+ROW_NUMBER() OVER (
+    PARTITION BY payments.customer_id, payments.amount
+    ORDER BY payments.payment_id
+) AS row_num
+FROM customers
+INNER JOIN payments
+ON customers.customer_id = payments.customer_id;
+
+SELECT first_name, amount
+FROM (
+    SELECT customers.first_name, payments.amount,
+    ROW_NUMBER() OVER (
+        PARTITION BY payments.customer_id, payments.amount
+        ORDER BY payments.payment_id
+    ) AS row_num
+    FROM customers
+    INNER JOIN payments
+    ON customers.customer_id = payments.customer_id
+) AS ranked_data
+WHERE row_num = 1;
