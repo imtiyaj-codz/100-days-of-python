f2d0a1f58d802b12cf6c833fb38c5e9272a24f77
I-nullptr <imtiyaz.maniyar07@gmail.com>
2026-04-25 23:08:00 +0530

    Create customers and payments tables with sample data
    Create tables for customers and payments with sample data and a query to retrieve first and last payment amounts.

---
+++ customer_first_last_payment.sql
@@ -0,0 +1,39 @@
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50)
+);
+
+CREATE TABLE payments (
+    payment_id INT PRIMARY KEY,
+    customer_id INT,
+    amount INT
+);
+
+INSERT INTO customers VALUES
+(1, 'Imtiyaz', 'Shaikh'),
+(2, 'Rahul', 'Patil'),
+(3, 'Sara', 'Khan'),
+(4, 'Aman', 'Verma');
+
+INSERT INTO payments VALUES
+(1, 1, 100),
+(2, 1, 300),
+(3, 1, 500),
+(4, 2, 200),
+(5, 2, 400),
+(6, 3, 250);
+
+SELECT customers.first_name, payments.payment_id, payments.amount,
+FIRST_VALUE(payments.amount) OVER (
+    PARTITION BY customers.customer_id
+    ORDER BY payments.payment_id
+) AS first_payment,
+LAST_VALUE(payments.amount) OVER (
+    PARTITION BY customers.customer_id
+    ORDER BY payments.payment_id
+    ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+) AS last_payment
+FROM customers
+INNER JOIN payments
+ON customers.customer_id = payments.customer_id;
